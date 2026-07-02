7a7304a7c8d7189e7c90575f7a6d8c56d92ccaa2
Mohit Tiwari <mmjsmohit@gmail.com>
2026-07-03 04:59:38 +0530

    feat: display similar movies in movie detail screen

--- cinebook_user_app/lib/screens/movie_detail_screen.dart
+++ cinebook_user_app/lib/screens/movie_detail_screen.dart
@@ -14,6 +14,7 @@ class MovieDetailScreen extends StatefulWidget {
 class _MovieDetailScreenState extends State<MovieDetailScreen> {
   Map<String, dynamic>? _movie;
   List<dynamic> _reviews = [];
+  List<dynamic> _similarMovies = [];
   bool _isLoading = true;
 
   @override
@@ -25,11 +26,15 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
   Future<void> _fetchDetails() async {
     try {
       final api = context.read<ApiClient>();
-      final res = await api.dio.get('/movies/${widget.movieId}');
-      final revRes = await api.dio.get('/movies/${widget.movieId}/reviews');
+      final results = await Future.wait([
+        api.dio.get('/movies/${widget.movieId}'),
+        api.dio.get('/movies/${widget.movieId}/reviews'),
+        api.dio.get('/movies/${widget.movieId}/similar'),
+      ]);
       setState(() {
-        _movie = res.data['movie'];
-        _reviews = revRes.data['reviews'];
+        _movie = results[0].data['movie'];
+        _reviews = results[1].data['reviews'];
+        _similarMovies = results[2].data['movies'];
         _isLoading = false;
       });
     } catch (e) {
@@ -67,6 +72,41 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
                   subtitle: Text(r['body'] ?? ''),
                   trailing: Text('${r['rating']} ★'),
                 )),
+                const SizedBox(height: 24),
+                if (_similarMovies.isNotEmpty) ...[
+                  Text('Similar Movies', style: Theme.of(context).textTheme.titleLarge),
+                  const SizedBox(height: 12),
+                  SizedBox(
+                    height: 160,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: _similarMovies.length,
+                      itemBuilder: (context, index) {
+                        final sm = _similarMovies[index];
+                        return GestureDetector(
+                          onTap: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => MovieDetailScreen(movieId: sm['id']))),
+                          child: Container(
+                            width: 110,
+                            margin: const EdgeInsets.only(right: 12),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: sm['posterUrl'] != null 
+                                    ? Image.network(sm['posterUrl'], height: 120, width: 110, fit: BoxFit.cover, errorBuilder: (context, error, stackTrace) => Container(height: 120, width: 110, color: CinemaColors.deepCharcoal))
+                                    : Container(height: 120, width: 110, color: CinemaColors.deepCharcoal),
+                                ),
+                                const SizedBox(height: 4),
+                                Text(sm['title'] ?? '', maxLines: 1, overflow: TextOverflow.ellipsis, style: Theme.of(context).textTheme.labelMedium),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ],
             ),
           ),
